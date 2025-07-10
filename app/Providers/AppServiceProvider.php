db1bd30c64bb224c8aa6232452d34abcae25b038
juanperez95 <jplesmes19@gmail.com>
2025-07-10 14:19:48 -0500

    ajuste del content mixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        Route::middleware('api')
-            ->prefix('api')
-            ->group(base_path('routes/api.php'));
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
